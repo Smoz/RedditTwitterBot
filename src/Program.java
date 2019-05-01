1b7d5c4ce1dc45de0703266a43f288d60af39d77
MariahS <smith.mariah@live.com>
2019-05-01 18:02:15 -0400

    TwitterService updated

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,12 @@
-import services.JsonService;
+import services.TwitterService;
 
 public class Program {
 
 	public static void main(String[] args) throws Exception {
-		 JsonService jsonService = new JsonService();
-		 jsonService.readRedditJson("https://www.reddit.com/r/popular/.json"); 
+		 //JsonService jsonService = new JsonService();
+		 //jsonService.readRedditJson("https://www.reddit.com/r/popular/.json"); 
+		
+		TwitterService twitterService = new TwitterService();
+		twitterService.PublishTweet("Hello World 2");
     }
 }
--- src/services/TwitterService.java
+++ src/services/TwitterService.java
@@ -1,14 +1,15 @@
 package services;
 
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
 public class TwitterService {
 
 	// Post tweet 
-	public void PublishTweet(){
-		
-	}
-	
-	// Connect to twitter
-	public void AuthenticateUser() {
-		
+	public void PublishTweet(String message) throws TwitterException{
+		Twitter twitter = TwitterFactory.getSingleton();
+		Status status = twitter.updateStatus(message);
 	}
 }
